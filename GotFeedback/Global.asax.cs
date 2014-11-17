3980f0c911af26a592b24e0a3c2e07ddafbd2fed
Simon Gilquin <simon@gilquin.com>
2014-11-17 12:44:54 +0100

    Merged remote and main

--- GotFeedback/Global.asax.cs
+++ GotFeedback/Global.asax.cs
@@ -18,6 +18,7 @@ namespace GotFeedback
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Bootstrapper.Initialize();
         }
     }
 }
