609b70622ed7697b1ffa1aef570045defde979f9
strickos9 <zarrai.oussema@hotmail.fr>
2014-11-17 11:55:28 +0100

    Added Entity Domain Added User Repository Service

---
+++ GotFeedback/Domain/Topic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GotFeedback.Domain
+{
+    public class Topic
+    {
+        public int Id { get; set; }
+
+        public string Message { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+    }
+}
---
+++ GotFeedback/Services/UserService.cs
@@ -0,0 +1,17 @@
+﻿using GotFeedback.Domain;
+
+namespace GotFeedback.Services
+{
+    public class UserService
+    {
+        public User Get(string userId)
+        {
+            return null;
+        }
+
+        public User GetByEmail(string email)
+        {
+            return null;
+        }
+    }
+}
