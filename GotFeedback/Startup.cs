384d064a5f0a91c5b3ad66c93cf439e1192a2d95
Simon Gilquin <simon@gilquin.com>
2014-11-17 10:00:45 +0100

    First commit

---
+++ GotFeedback/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GotFeedback.Startup))]
+namespace GotFeedback
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
