bde4bebef727d5b64c8b5e86ba5df2a3804f9006
Simon Gilquin <simon@gilquin.com>
2014-11-18 09:23:12 +0100

    SignalR + Hub

---
+++ GotFeedback/Hubs/TopicHub.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using GotFeedback.Models;
+using Microsoft.AspNet.SignalR;
+
+namespace GotFeedback.Hubs
+{
+    public class TopicHub : Hub
+    {
+        private readonly ApplicationDbContext db;
+
+        public TopicHub()
+        {
+            db = new ApplicationDbContext();
+        }
+
+        public void TopicCommented(int id)
+        {
+            var usersOnTopic = db.Topics.Select(t => new {t.User.UserName, t.Id}).SingleOrDefault(t => t.Id == id);
+            if (usersOnTopic != null)
+            {
+                Clients.User(usersOnTopic.UserName).notify();
+            }
+        }
+    }
+}
--- GotFeedback/Startup.cs
+++ GotFeedback/Startup.cs
@@ -9,6 +9,7 @@ namespace GotFeedback
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            app.MapSignalR();
         }
     }
 }
